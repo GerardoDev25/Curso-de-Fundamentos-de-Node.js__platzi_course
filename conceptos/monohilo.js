10338c2806d4a784db7e26f5acf8fe22493fe047
Gerardo Miranda <gerardodev25@gmail.com>
2022-11-03 22:25:13 -0400

    Monohilo: implicaciones en diseño y seguridad

---
+++ conceptos/monohilo.js
@@ -0,0 +1,12 @@
+console.log('hola mundo');
+
+let i = 0;
+
+setInterval(() => {
+  console.log(i++);
+  // if (i == 5) {
+  //   var a = 3 + s;
+  // }
+}, 1000);
+
+console.log('segunda instruccion');
