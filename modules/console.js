fd1f43fd2a1cb24ce307f4b524b3e16666e6d2fd
Gerardo Miranda <gerardodev25@gmail.com>
2022-11-04 22:42:23 -0400

    Console

---
+++ modules/console.js
@@ -0,0 +1,45 @@
+// const table = {
+//   a: { a: 1, b: 1 },
+//   b: { a: 1, b: 1 },
+// };
+// console.table(table);
+
+// console.group('conversacion');
+// console.log('sdfsfsdfsdfsfds');
+// console.log('sdfsfsdfsdfsfds');
+// console.log('sdfsfsdfsdfsfds');
+// console.log('sdfsfsdfsdfsfds');
+// console.log('sdfsfsdfsdfsfds');
+// console.groupEnd('conversacion');
+// console.log('otra cosa');
+
+// function fun1() {
+//   console.group('function 1')
+//   console.log('this is fron fun 1');
+//   console.log('this too');
+//   fun2();
+//   console.log('we go back to 1');
+//   console.groupEnd('function 1')
+// }
+
+// function fun2() {
+//   console.group('function 2')
+//   console.log('now we are at the fun 2');
+//   console.groupEnd('function 2')
+
+// }
+// console.log('empezamos');
+// fun1()
+
+console.count('veces');
+console.count('veces');
+console.count('veces');
+console.count('veces');
+console.count('veces');
+console.count('veces');
+console.count('veces');
+console.count('veces');
+console.count('veces');
+console.count('veces');
+console.countReset('veces');
+console.count('veces');
