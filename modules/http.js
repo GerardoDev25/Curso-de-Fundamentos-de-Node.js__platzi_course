2886aa838d34cf9ea649d83a645171eb380d00a9
Gerardo Miranda <gerardodev25@gmail.com>
2022-11-06 21:31:08 -0400

    debugger

--- modules/http.js
+++ modules/http.js
@@ -3,31 +3,31 @@ const http = require('http');
 http.createServer(router).listen(3000);
 
 function router(req, res) {
-    console.log('Nueva petición!');
-    console.log(req.url);
+  console.log('Nueva petición!');
+  console.log(req.url);
 
-    switch (req.url) {
-        case '/hola':
-            let saludo = hola();
-            res.write(saludo);
-            res.end();
-            break;
-        
-        default:
-            res.write('Error 404: No se lo que quieres');
-            res.end();
-    }
+  switch (req.url) {
+    case '/hola':
+      let saludo = hola();
+      res.write(saludo);
+      res.end();
+      break;
 
-    // res.writeHead(201, { 'Content-Type': 'text/plain' })
+    default:
+      res.write('Error 404: No se lo que quieres');
+      res.end();
+  }
 
-    // // Escribir respuesta al usuario
-    // res.write('Hola, ya se usar HTTP de NodeJS');
+  // res.writeHead(201, { 'Content-Type': 'text/plain' })
 
-    // res.end();
+  // // Escribir respuesta al usuario
+  // res.write('Hola, ya se usar HTTP de NodeJS');
+
+  // res.end();
 }
 
 function hola() {
-    return 'Hola, que tal';
+  return 'Hola, que tal';
 }
 
-console.log("Escuchando http en el puerto 3000");
+console.log('Escuchando http en el puerto 3000');
